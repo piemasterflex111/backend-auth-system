ba4d70f89d3d1c7a1443680c0adb4c0efe76a8c3
Payam Adloo <payamadloo@gmail.com>
2026-02-27 14:24:02 -0800

    refactor(test): use bcrypt hashing for test user creation

--- test_insert.py
+++ test_insert.py
@@ -1,16 +1,18 @@
 from app.db.session import SessionLocal
 from app.models.user import User
+from app.security import get_password_hash
 
 db = SessionLocal()
 
 new_user = User(
-    email="payamistestinguserobject@gmail.com",
-    hashed_password="fakehashedpassword",
+    email="payamistestinguserobject_2-27-26@gmail.com",
+    hashed_password=get_password_hash("test123"),
 )
 
 db.add(new_user)
 db.commit()
-users = db.query(User).all()    
+
+users = db.query(User).all()
 print(users)
 
-db.close()  
+db.close()
